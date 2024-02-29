b8929ab4db7ab90220086eb2a08e2a07b375c2b5
ecyr20 <ecyr20@tamu.edu>
2024-02-29 12:47:07 -0600

    Implemented Routes for Family Model
    Added CRUD routes for family model

--- Services/FamilyService.cs
+++ Services/FamilyService.cs
@@ -16,6 +16,54 @@ namespace Chemistry_Cafe_API.Services
             return await ReadAllAsync(await command.ExecuteReaderAsync());
         }
 
+        public async Task<Family?> GetFamilyAsync(int uuid)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"SELECT * FROM Family WHERE uuid = @id";
+            command.Parameters.AddWithValue("@id", uuid);
+
+            var result = await ReadAllAsync(await command.ExecuteReaderAsync());
+            return result.FirstOrDefault();
+        }
+
+        public async Task CreateFamilyAsync(string name)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"INSERT INTO Family (name) VALUES (@name);";
+            command.Parameters.AddWithValue("@name", name);
+
+            await command.ExecuteNonQueryAsync();
+        }
+        public async Task UpdateFamilyAsync(Family family)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"UPDATE Family SET name = @name, isDel = @isDel WHERE uuid = @uuid;";
+            
+            command.Parameters.AddWithValue("@uuid", family.uuid);
+            command.Parameters.AddWithValue("@name", family.name);
+            command.Parameters.AddWithValue("@isDel", family.isDel);
+
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task DeleteFamilyAsync(int uuid)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"UPDATE Family SET isDel = 1 WHERE uuid = @uuid;";
+
+            command.Parameters.AddWithValue("@uuid", uuid);
+            
+            await command.ExecuteNonQueryAsync();
+        }
+
         private async Task<IReadOnlyList<Family>> ReadAllAsync(DbDataReader reader)
         {
             var families = new List<Family>();
