0da2a110d07fe17fa67cac59e2053cdf3e80bf44
ecyr20 <ecyr20@tamu.edu>
2024-03-07 13:06:58 -0600

    Added The TagMechanism Species and Reactions List

---
+++ Controllers/TagMechanismReactionListController.cs
@@ -0,0 +1,57 @@
+﻿using Chemistry_Cafe_API.Models;
+using Chemistry_Cafe_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Chemistry_Cafe_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagMechanismReactionListController : ControllerBase
+    {
+        private TagMechanismReactionListService mechTagMechService;
+        
+        //Injects sql data source setup in Program.cs
+        public TagMechanismReactionListController([FromServices] MySqlDataSource db)
+        {
+            this.mechTagMechService = new TagMechanismReactionListService(db);
+        }
+
+        // GET: api/TagMechanismReaction/all
+        [HttpGet("all")]
+        public async Task<IReadOnlyList<TagMechanismReactionList>> Get()
+        {
+            return await mechTagMechService.GetTagMechanismReactionsAsync();
+        }
+
+        // GET api/TagMechanismReaction/5
+        [HttpGet("{uuid}")]
+        public async Task<TagMechanismReactionList?> Get(Guid uuid)
+        {
+            return await mechTagMechService.GetTagMechanismReactionAsync(uuid);
+        }
+
+        // POST api/TagMechanismReaction/create
+        [HttpPost("create")]
+        public async Task Create([FromBody] TagMechanismReactionList newTagMechanismReaction)
+        {
+            await mechTagMechService.CreateTagMechanismReactionAsync(newTagMechanismReaction);
+        }
+
+        // PUT api/TagMechanismReaction/5
+        [HttpPut("update")]
+        public async Task Put([FromBody] TagMechanismReactionList newTagMechanismReaction)
+        {
+            await mechTagMechService.UpdateTagMechanismReactionAsync(newTagMechanismReaction);
+        }
+
+        // DELETE api/TagMechanismReaction/delete/5
+        [HttpDelete("delete/{uuid}")]
+        public async Task Delete(Guid uuid)
+        {
+            await mechTagMechService.DeleteTagMechanismReactionAsync(uuid);
+        }
+    }
+}
---
+++ Services/TagMechanismReactionListService.cs
@@ -0,0 +1,96 @@
+﻿using Chemistry_Cafe_API.Models;
+using System.Data.Common;
+using MySqlConnector;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chemistry_Cafe_API.Services
+{
+    public class TagMechanismReactionListService (MySqlDataSource database)
+    {
+        public async Task<IReadOnlyList<TagMechanismReactionList>> GetTagMechanismReactionsAsync()
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = "SELECT * FROM Mechanism_TagMechanism";
+            return await ReadAllAsync(await command.ExecuteReaderAsync());
+        }
+
+        public async Task<TagMechanismReactionList?> GetTagMechanismReactionAsync(Guid uuid)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"SELECT * FROM Mechanism_TagMechanism WHERE uuid = @id";
+            command.Parameters.AddWithValue("@id", uuid);
+
+            var result = await ReadAllAsync(await command.ExecuteReaderAsync());
+            return result.FirstOrDefault();
+        }
+
+        public async Task CreateTagMechanismReactionAsync(TagMechanismReactionList newTagMechanismReaction)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            Guid tagMechanismReactionID = Guid.NewGuid();
+
+            command.CommandText = @"INSERT INTO Mechanism_TagMechanism (uuid, reaction_uuid, tag_mechanism_uuid, version) VALUES (@uuid, @reaction_uuid, @tag_mechanism_uuid, @version);";
+
+            command.Parameters.AddWithValue("@uuid", tagMechanismReactionID);
+            command.Parameters.AddWithValue("@reaction_uuid", newTagMechanismReaction.reaction_uuid);
+            command.Parameters.AddWithValue("@tag_mechanism_uuid", newTagMechanismReaction.tag_mechanism_uuid);
+            command.Parameters.AddWithValue("@version", newTagMechanismReaction.version);
+
+            await command.ExecuteNonQueryAsync();
+        }
+        public async Task UpdateTagMechanismReactionAsync(TagMechanismReactionList tagMechanismReaction)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"UPDATE Mechanism_TagMechanism SET reaction_uuid = @reaction_uuid, tag_mechanism_uuid = @tag_mechanism_uuid, version = @version, isDel = @isDel WHERE uuid = @uuid;";
+            
+            command.Parameters.AddWithValue("@uuid", tagMechanismReaction.uuid);
+            command.Parameters.AddWithValue("@reaction_uuid", tagMechanismReaction.reaction_uuid);
+            command.Parameters.AddWithValue("@tag_mechanism_uuid", tagMechanismReaction.tag_mechanism_uuid);
+            command.Parameters.AddWithValue("@version", tagMechanismReaction.version);
+            command.Parameters.AddWithValue("@isDel", tagMechanismReaction.isDel);
+
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task DeleteTagMechanismReactionAsync(Guid uuid)
+        {
+            using var connection = await database.OpenConnectionAsync();
+            using var command = connection.CreateCommand();
+
+            command.CommandText = @"UPDATE Mechanism_TagMechanism SET isDel = 1 WHERE uuid = @uuid;";
+
+            command.Parameters.AddWithValue("@uuid", uuid);
+            
+            await command.ExecuteNonQueryAsync();
+        }
+
+        private async Task<IReadOnlyList<TagMechanismReactionList>> ReadAllAsync(DbDataReader reader)
+        {
+            var tagMechanismReaction = new List<TagMechanismReactionList>();
+            using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var mechTag = new TagMechanismReactionList
+                    {
+                        uuid = reader.GetGuid(0),
+                        reaction_uuid = reader.GetGuid(1),
+                        tag_mechanism_uuid = reader.GetGuid(2),
+                        version = reader.GetString(3),
+                        isDel = reader.GetBoolean(4),
+                    };
+                    tagMechanismReaction.Add(mechTag);
+                }
+            }
+            return tagMechanismReaction;
+        }
+    }
+}
