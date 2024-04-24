c5d93df3519b2c4ebd4ee736d8bb60947b7b6056
ecyr20 <ecyr20@tamu.edu>
2024-04-24 12:10:43 -0500

    Initial Code Coverage tool

---
+++ Tests/FamilyControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Chemistry_Cafe_API.Controllers;
+using Chemistry_Cafe_API.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySqlConnector;
+
+namespace Chemistry_Cafe_API.Tests
+{
+    [TestClass]
+    public class FamilyControllerTests
+    {
+        readonly MySqlDataSource db = new MySqlDataSource("Server=chemisty-cafe.cl8uuceq2rud.us-east-1.rds.amazonaws.com;User ID=cafeadmin;Password=cafeadmin;Port=3306;Database=Testing");
+        
+        [TestMethod]
+        public async Task Get_retrieves_family()
+        {
+            var controller = new FamilyController(db);
+
+            var result = await controller.Get() as List<Family>;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
